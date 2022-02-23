de23b1f2e807a8213eefc28b460ca41e54bcbe23
naga1 <naga1@192.168.1.5>
2022-02-23 21:41:33 +0530

    committed

--- src/test/java/TestPackage/TestSelenium.java
+++ src/test/java/TestPackage/TestSelenium.java
@@ -5,6 +5,17 @@ public class TestSelenium {
 	public static void main(String[] args) {
 	
 		System.out.println("TestSelenium");
+		
+		int intNumber = 10;
+		
+		
+		if (intNumber/2!=0) {
+			
+			System.out.println("Prime");
+		}else {
+			
+			System.out.println("Not Prime");
+		}
 
 	}
 
